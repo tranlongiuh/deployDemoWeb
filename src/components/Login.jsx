b8ecf20f64299aeb219ddde27e0bd5b85069daf6
Long <85337233+tranlongiuh@users.noreply.github.com>
2024-12-06 03:43:02 +0700

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
 			formData.append("username", form.userName);
 			formData.append("password", form.password);
 			const response = await axios.post(
-				"https://angelic-strength-production.up.railway.app/api/auth/login",
+				"https://canteen.iuh.api.com/api/auth/login",
 				formData,
 				{
 					headers: {
